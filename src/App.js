5611b786cd28869619a40e05398d2748cecb1947
Luana Pina <83229362+luana-pina@users.noreply.github.com>
2022-02-01 20:30:38 -0300

    feat: create a card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started</h2>
-    </div>
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
